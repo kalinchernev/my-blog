50c17f2a6e863ddfa5479a037c186a2e9c1f4a35
Kalin Chernev <kalin.chernev@gmail.com>
2018-04-29 16:33:41 +0200

    Small refactoring

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
+import styles from './styles.module.css'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -14,16 +15,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div className={styles.main}>{children()}</div>
   </div>
 )
 
