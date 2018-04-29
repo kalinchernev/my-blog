9a93011fce29b55a2c00dad9db1682c69fa84696
Kalin Chernev <kalin.chernev@gmail.com>
2018-04-29 13:37:36 +0200

    First plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'My Blog',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-typography`],
 }
