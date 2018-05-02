7bd45ff78c10edf1ea7a2fe2a006259ae3a90092
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-02 21:10:10 +0200

    And this one

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,46 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
- // You can delete this file if you're not using it
+exports.onCreateNode = ({ node, boundActionCreators }) => {
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = node.frontmatter.slug
+    const { createNodeField } = boundActionCreators
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      const posts = results.data.allMarkdownRemark.edges
+      posts.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          context: {
+            slug: node.fields.slug,
+          },
+          component: path.resolve('./src/templates/blog-post.js'),
+        })
+      })
+
+      resolve()
+    })
+  })
+}
